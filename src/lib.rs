835c37e763a17d7ea240bcd0dcdcef84dc41aaad
teoxoy <28601907+teoxoy@users.noreply.github.com>
2023-02-12 22:22:59 +0100

    use `doc_auto_cfg`

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(docs, feature(doc_cfg))]
+#![cfg_attr(docs, feature(doc_auto_cfg))]
 #![cfg_attr(coverage_nightly, feature(no_coverage))]
 #![deny(rustdoc::broken_intra_doc_links)]
 #![warn(
