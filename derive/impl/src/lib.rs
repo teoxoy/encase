726946d4343caf8d6f52c792cd0d1a4e1b0485d5
Bruce Reif (Buswolley) <btreif@gmail.com>
2023-03-27 15:52:44 -0700

    use meta_list span

--- derive/impl/src/lib.rs
+++ derive/impl/src/lib.rs
@@ -197,33 +197,41 @@ pub fn derive_shader_type(input: DeriveInput, root: &Path) -> TokenStream {
                 align: None,
             };
             for attr in &field.attrs {
-                let span = attr.to_token_stream().span();
-                if attr.path().is_ident("align") {
-                    let res = attr.parse_args::<AlignmentAttr>();
-                    let res = res.map_err(|err| syn::Error::new(span, err));
-                    match res {
-                        Ok(val) => data.align = Some((val.0, span)),
-                        Err(err) => errors.append(err),
-                    }
-                } else if attr.path().is_ident("size") {
-                    let res = if i == last_field_index {
-                        attr.parse_args::<SizeAttr>().map(|val| match val {
-                            SizeAttr::Runtime => {
-                                is_runtime_sized = true;
-                                None
+                match attr.meta.require_list() {
+                    Ok(meta_list) => {
+                        let path = &meta_list.path;
+                        let span = meta_list.tokens.span();
+                        if path.is_ident("align") {
+                            if path.is_ident("align") {
+                                let res = attr.parse_args::<AlignmentAttr>();
+                                let res = res.map_err(|err| syn::Error::new(span, err));
+                                match res {
+                                    Ok(val) => data.align = Some((val.0, span)),
+                                    Err(err) => errors.append(err),
+                                }
+                            } else if path.is_ident("size") {
+                                let res = if i == last_field_index {
+                                    attr.parse_args::<SizeAttr>().map(|val| match val {
+                                        SizeAttr::Runtime => {
+                                            is_runtime_sized = true;
+                                            None
+                                        }
+                                        SizeAttr::Static(size) => Some((size.0, span)),
+                                    })
+                                } else {
+                                    attr.parse_args::<StaticSizeAttr>()
+                                        .map(|val| Some((val.0, span)))
+                                };
+                                let res = res.map_err(|err| syn::Error::new(span, err));
+                                match res {
+                                    Ok(val) => data.size = val,
+                                    Err(err) => errors.append(err),
+                                }
                             }
-                            SizeAttr::Static(size) => Some((size.0, span)),
-                        })
-                    } else {
-                        attr.parse_args::<StaticSizeAttr>()
-                            .map(|val| Some((val.0, span)))
-                    };
-                    let res = res.map_err(|err| syn::Error::new(span, err));
-                    match res {
-                        Ok(val) => data.size = val,
-                        Err(err) => errors.append(err),
+                        }
                     }
-                }
+                    Err(err) => errors.append(err),
+                };
             }
             data
         })
